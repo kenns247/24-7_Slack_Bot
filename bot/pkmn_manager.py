056663d87bbbb0257ea501a3667d82c6f9d9ba9f
jhickson <jhickson@24-7intouch.com>
2016-11-26 18:34:26 -0600

    kill the spaces dead

--- bot/pkmn_manager.py
+++ bot/pkmn_manager.py
@@ -14,9 +14,9 @@ class PokemonManager(object):
     def whos_that_pkmn(self, channel):
         answer = self.correct_answers.pop(channel, None)
         if answer is None:
-			return self.random_pkmn(channel)
+            return self.random_pkmn(channel)
         else:
-            return 'It was {}. Guess you aren\'t a Pokemon Master.'.format(answer)			
+            return 'It was {}. Guess you aren\'t a Pokemon Master.'.format(answer)            
 
     def random_pkmn(self, channel):
         num = random.randint(1, 151)
@@ -33,21 +33,21 @@ class PokemonManager(object):
             return 'Who\'s that Pokemon? {}'.format(sprite)
 
     def check_response(self, user, channel, msg):
-		if (channel in self.correct_answers):
+        if (channel in self.correct_answers):
             answer = self.correct_answers[channel]
-			tokens = msg.split()
-			if answer in tokens:
-				return self.guessed_correctly(user, channel)
-			else:
-				return '<@{}> {}'.format(user, self.neg_response_manager.get_response())
+            tokens = msg.split()
+        if answer in tokens:
+            return self.guessed_correctly(user, channel)
+            else:
+                return '<@{}> {}'.format(user, self.neg_response_manager.get_response())
 
     def guessed_correctly(self, user, channel):
         random_response = self.pos_response_manager.get_response()
         revealed_name = self.reveal_answer()
         return '{} {} You go <@{}>!'.format(random_response, revealed_name, user)
-		
-	def choose_pkmn(self, target):
-		link = URL
+        
+    def choose_pkmn(self, target):
+        link = URL
         pkmn = link.format(target)
         try:
             response = requests.get(pkmn)
