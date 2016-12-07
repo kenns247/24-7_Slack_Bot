1ed31eb6b75de0ee44333507b959a0cdae0d848f
Kiera Enns <kenns@24-7intouch.com>
2016-12-07 14:19:04 -0600

    committing the perfect ratio

--- bot/time_triggered_event_manager.py
+++ bot/time_triggered_event_manager.py
@@ -42,7 +42,7 @@ class TimeTriggeredEventManager(object):
         self.msg_writer.send_message(channel_id, response)
 
     def trigger_wake_me_up(self):
-        if random.random() < 0.001:
+        if random.random() < 0.000000000001:
             global HAS_TRIGGERED_THIS_HOUR
             HAS_TRIGGERED_THIS_HOUR = True
             channel_id = self.channel_manager.get_channel_id('general')
